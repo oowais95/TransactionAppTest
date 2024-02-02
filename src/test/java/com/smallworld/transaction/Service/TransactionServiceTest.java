85480a482f95e840816a716c257633b99fbd0e00
oowais95 <45528844+oowais95@users.noreply.github.com>
2024-02-02 10:17:27 +0500

    proper manual test cases - earlier ones were automated
    proper manual test cases - earlier ones were automated

---
+++ src/test/java/com/smallworld/transaction/Service/TransactionServiceTest.java
@@ -0,0 +1,88 @@
+package com.smallworld.transaction.Service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.smallworld.transaction.Service.TransactionService;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+public class TransactionServiceTest {
+
+
+    @Autowired
+    TransactionService transactionService;
+
+    @Test
+    void test(){
+        System.out.println("test ran");
+    }
+
+
+    @Test
+    public void MaxTransactionAmountTest() throws IOException {
+
+        double expectedMaxAmount = 985;
+
+         double actualSum = transactionService.getMaxTransactionAmount();
+
+        // Assert
+        Assertions.assertEquals(expectedMaxAmount, actualSum, "Result is incorrect");
+    }
+
+
+
+    @Test
+    public void topSenderTest() throws IOException {
+
+        String expected = "Grace Burgess";
+
+        // Act
+        String actual = transactionService.getTopSender();
+
+        // Assert
+        Assertions.assertEquals(expected, actual, "Result  is incorrect");
+    }
+
+
+    @Test
+    public void getTotalTransactionAmount() throws IOException {
+
+        double expected = 4371.37;
+
+        // Act
+        double actual = transactionService.getTotalTransactionAmount();
+
+        // Assert
+        Assertions.assertEquals(expected, actual, "Result  is incorrect");
+    }
+
+    @Test
+    public void countUniqueClients() throws IOException {
+
+        long expected = 14;
+
+        // Act
+        long actual = transactionService.countUniqueClients();
+
+        // Assert
+        Assertions.assertEquals(expected, actual, "Result  is incorrect");
+    }
+
+
+    @Test
+    public void countUniqueClients() throws IOException {
+
+        long expected = 14;
+
+        // Act
+        long actual = transactionService.countUniqueClients();
+
+        // Assert
+        Assertions.assertEquals(expected, actual, "Result  is incorrect");
+    }
+
+
+}
